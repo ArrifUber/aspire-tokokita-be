d65a1bbc8a1ac769627fa3532922ab4e96b14fa9
iLoveOrca1 <135596149+iLoveOrca1@users.noreply.github.com>
2026-09-10 17:45:30 +0800

    add: baru create get cik woiylan

--- internal/app/domain/vendor/dto.js
+++ internal/app/domain/vendor/dto.js
@@ -1,26 +1,31 @@
-const CreateVendorRequest = (data) => ({
-  id: data.id,
-  name: data.name,
-  picName: data.picName,
-  picPhone: data.picPhone,
-  rekening: data.rekening,
-  noRekening: data.noRekening,
-  jumlahProduk: data.jumlahProduk,
-  isActive: true,
-})
+const CreateVendorRequest = (data) => {
+  if (!data.name) throw { statusCode: 400, message: "Vendor name is required" };
+  return {
+    id: data.id,
+    name: data.name,
+    picName: data.picName,
+    picPhone: data.picPhone,
+    rekening: data.rekening,
+    noRekening: data.noRekening,
+    jumlahProduk: data.jumlahProduk,
+    isActive: true,
+  };
+};
 
-const CreateVendorResponse = (data) => ({
-  id: data.id,
-  name: data.name,
-  picName: data.picName,
-  picPhone: data.picPhone,
-  rekening: data.rekening,
-  noRekening: data.noRekening,
-  jumlahProduk: data.jumlahProduk,
-  isActive: true,
-  createdAt: data.createdAt,
-  updatedAt: data.updatedAt
-})
+const CreateVendorResponse = (data) => {
+  return {
+    id: data.id,
+    name: data.name,
+    picName: data.picName,
+    picPhone: data.picPhone,
+    rekening: data.rekening,
+    noRekening: data.noRekening,
+    jumlahProduk: data.jumlahProduk,
+    isActive: true,
+    createdAt: data.createdAt,
+    updatedAt: data.updatedAt
+  };
+};
 
 module.exports = {
   CreateVendorRequest,
--- internal/app/domain/vendor/service.js
+++ internal/app/domain/vendor/service.js
@@ -1,16 +1,21 @@
 const repository = require("./respository");
+const {   
+  CreateVendorRequest,
+  CreateVendorResponse 
+} = require('./dto')
 
 const getAll = async () => {
   return await repository.getAll();
 }
 
 const create = async (data) => {
-  return await repository.create(data);
+  const payload = CreateVendorRequest(data);
+  return await repository.create(payload);
 }
 
-const edit = async (id, data) => {
-  return await repository.edit(id, data);
-}
+// const edit = async (id, data) => {
+//   return await repository.edit(id, data);
+// }
 
 module.exports = { 
   getAll, 
