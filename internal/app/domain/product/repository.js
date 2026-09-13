1090fa6307a2ea277d02261932e7529b61f33583
ArrifUber <arifbudianto299@gmail.com>
2026-09-13 20:29:46 +0700

    Fix vendor selection in findById method to ensure correct data retrieval

--- internal/app/domain/product/repository.js
+++ internal/app/domain/product/repository.js
@@ -40,6 +40,12 @@ const findById = async (id) => {
           name: true,
         },
       },
+            vendor: {
+        select: {
+          id: true,
+          name: true,
+        },
+      },
     },
   });
 };
